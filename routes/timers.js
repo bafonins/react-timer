a9151fa862e3fc927a5209308114afb94180b7f9
Bogdans Afonins <bogdans.afonins@gmail.com>
2018-02-05 21:24:21 +0100

    setup node server and implemented basic CRUD

---
+++ routes/timers.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const router = express.Router();
+const resolve = path.resolve;
+
+const FILE_PATH = resolve('mock-data.json');
+
+router.get('/', (req, res, next) => {
+    fs.readFile(FILE_PATH, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500);
+        } else {
+            res.setHeader('Cache-Control', 'no-cache');
+            res.json(JSON.parse(data));
+        }
+    });
+});
+
+router.post('/', (req, res, next) => {
+    fs.readFile(FILE_PATH, (err, data) => {
+        const timer = {
+            title: req.body.title,
+            project: req.body.project,
+            id: req.body.id,
+            elapsed: 0,
+            runningSince: null,
+        };
+        const timers = JSON.parse(data);
+        timers.push(timer);
+
+        fs.writeFile(FILE_PATH, JSON.stringify(timers, null, 4), (err) => {
+            if (err) {
+                console.log(err);
+                res.status(500);
+            } else {
+                res.setHeader('Cache-Control', 'no-cache');
+                res.json(timers);
+            }
+        });
+    });
+});
+
+router.put('/', (req, res, next) => {
+    fs.readFile(FILE_PATH, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500);
+        } else {
+            const timers = JSON.parse(data);
+            const id = req.body.id;
+            const title = req.body.title;
+            const project = req.body.project;
+
+            timers.forEach(t => {
+                if (t.id === id) {
+                    t.title = title;
+                    t.project = project;
+                }
+            });
+
+            fs.writeFile(FILE_PATH, JSON.stringify(timers, null, 4), (err) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500);
+                } else {
+                    res.json({});
+                }
+            });
+        }
+    });
+});
+
+router.delete('/', (req, res) => {
+    fs.readFile(FILE_PATH, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500);
+        } else {
+            let timers = JSON.parse(data);
+            timers = timers.reduce((memo, timer) => {
+                if (timer.id === req.body.id) {
+                    return memo;
+                } else {
+                    return memo.concat(timer);
+                }
+            }, []);
+
+            fs.writeFile(FILE_PATH, JSON.stringify(timers, null, 4), (err) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500);
+                } else {
+                    res.json({});
+                }
+            });
+        }
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const fs = require('fs');
 const bodyParser = require('body-parser');
-const path = require('path');
+const timersRouter = require('./routes/timers');
 
 const app = express();
-const FILE_PATH = path.join(__dirname, 'mock-data.json');
 const PORT = process.env.PORT || 3001;
 
 // app.use('/', express.static(path.join(__dirname, 'client', 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use('/api/timers', timersRouter);
 
 app.listen(PORT, (err) => {
     if (err) {
